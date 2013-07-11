fa3d897b55f6314e33b0b4ca8f0698398f1dfb1b
Xavier Lange <xrlange@gmail.com>
2013-07-11 13:58:23 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SdrubyApp::Application.config.secret_key_base = '7b8f84f07766977a42a7d5bbf30752c4125ea96eb9b9be83395aa066ae11c5f8cbc8bbebcfc120ae8d30a2ffcd6f02eb22aecf61f2562991919f5ec013fe861d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SdrubyApp::Application.config.session_store :cookie_store, key: '_sdruby-app_session'
